d36a3120a015706118c63bcddfefc22b370ac96a
Viet <lacvietnvt2002@gmail.com>
2022-08-27 23:46:57 +0700

    create Menu for fast and verified

---
+++ OnlineVoting/app/src/main/java/com/example/onlinevoting/FastMenu.java
@@ -0,0 +1,22 @@
+package com.example.onlinevoting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class FastMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fast_menu);
+    }
+
+    public void fastjoinclick(View view) {
+
+    }
+
+    public void fasthostclick(View view) {
+    }
+}
