37f4e5e1cdfc41287b769e051bb5edb36b4b5c16
Viet <lacvietnvt2002@gmail.com>
2022-08-28 20:23:33 +0700

    join activity

---
+++ OnlineVoting/app/src/main/java/com/example/onlinevoting/JoinActivity.java
@@ -0,0 +1,19 @@
+package com.example.onlinevoting;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+
+    public void oncheckfastjoin(View view) {
+
+    }
+}
