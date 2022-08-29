b33d4dcd6d8ae23c058e07070173ff4ee6377bbb
Viet <lacvietnvt2002@gmail.com>
2022-08-29 23:42:08 +0700

    add initial test

--- OnlineVoting/app/src/main/java/com/example/onlinevoting/JoinActivity.java
+++ OnlineVoting/app/src/main/java/com/example/onlinevoting/JoinActivity.java
@@ -2,6 +2,8 @@ package com.example.onlinevoting;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,9 +13,27 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+        EditText pollID= findViewById(R.id.editfastpollid);
+        EditText pollpass=findViewById(R.id.editfastpollpass);
+        Button joinbtn=findViewById(R.id.checkfastjoinbtn);
+        joinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pID= pollID.getText().toString().trim();
+                String pPass= pollpass.getText().toString().trim();
+                if(pID.isEmpty()){
+                    pollID.setError("Poll ID is required");
+                    pollID.requestFocus();
+                    return;
+                }
+                if(pPass.isEmpty()){
+                    pollpass.setError("Poll password is required");
+                    pollpass.requestFocus();
+                    return;
+                }
+            }
+        });
     }
 
-    public void oncheckfastjoin(View view) {
 
-    }
 }
--- OnlineVoting/app/src/main/java/com/example/onlinevoting/LoginActivity.java
+++ OnlineVoting/app/src/main/java/com/example/onlinevoting/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.onlinevoting;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,7 +13,6 @@ import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
-
     ImageView googleBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +20,31 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         TextView ForgetPass= findViewById(R.id.forgotpass);
 //        ForgetPass.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
 
         MaterialButton loginBtn = findViewById(R.id.loginbtn);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String u = username.getText().toString().trim();
+                String p= password.getText().toString().trim();
+                if(u.isEmpty()){
+                    username.setError("User name is required");
+                    username.requestFocus();
+                    return;
+                }
+                if(p.isEmpty()){
+                    password.setError("Password is required");
+                    password.requestFocus();
+                    return;
+                }
+                if(p.length()<4){
+                    password.setError("Password must longer than 4 digits");
+                    password.requestFocus();
+                    return;
+                }
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     //correct
                     Toast.makeText(LoginActivity.this,"Welcome",Toast.LENGTH_SHORT).show();
